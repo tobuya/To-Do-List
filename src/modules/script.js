ae4abaf63a46ab853acc56c135d1e8b79b04211c
tobuya <thomasobuya95@yahoo.com>
2022-12-15 13:21:02 +0300

    Replace the ul with checkbox to match the design

--- src/modules/script.js
+++ src/modules/script.js
@@ -1,4 +1,5 @@
 const listData = document.querySelector('.lists');
+const inputField = document.querySelector('.add-to-list');
 
 const tasks = [
   {
@@ -29,15 +30,31 @@ const tasks = [
 ];
 
 function addToList() {
-  for (let i = 0; i < tasks.length; i += 1) {
-    listData.innerHTML += `
-      <ul class="main-list" style="list-style-type: none">
-        <li>${tasks[i].index}</li>
-        <li>${tasks[i].description}</li>
-        <li>${tasks[i].completed}</li>
-      </ul>
-    `;
-  }
+  listData.innerHTML = '';
+  tasks.forEach((element) => {
+    if (element.completed === true) {
+      listData.innerHTML += `
+      <div class="main-list">
+      <input type="checkbox" class="check" id="check${element.index}" onclick="Check(${element.index});" checked>
+      <input class="list-item" id="list${element.index}" value="${element.description}" readonly>
+      <i class="fa-solid fa-pen-to-square edit " id="edit${element.index}" onclick="editList(${element.index});"></i>
+           <i class="fa-solid fa-floppy-disk save hide" id="save${element.index}" onclick="saveList(${element.index});"></i>
+      <i id="remove-icon" onclick="Remove(${element.index});" class="fa-solid fa-trash"></i>
+      </div>
+     `;
+    } else {
+      listData.innerHTML += `
+      <div class="main-list">
+      <input type="checkbox" class="check" id="check${element.index}" onclick="Check(${element.index});">
+      <input class="list-item" id="list${element.index}" value="${element.description}" readonly>
+      <i class="fa-solid fa-pen-to-square edit " id="edit${element.index}" onclick="editList(${element.index});"></i>
+           <i class="fa-solid fa-floppy-disk save hide" id="save${element.index}" onclick="saveList(${element.index});"></i>
+      <i id="remove-icon" onclick="Remove(${element.index});" class="fa-solid fa-trash"></i>
+      </div>
+     `;
+    }
+    inputField.value = '';
+  });
 }
 
 window.addEventListener('load', addToList);
